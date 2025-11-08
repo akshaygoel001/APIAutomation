2e3b865caed2bbab5afbd7fcfd18ea71ab5a2020
AkshayG <goelakshay001@gmail.com>
2025-11-08 16:33:25 +0530

    Restructure folder and modified validatePostWithString method

--- src/main/java/utils/JSONReader.java
+++ src/main/java/utils/JSONReader.java
@@ -11,22 +11,30 @@ import java.io.IOException;
 
 public class JSONReader {
 
-    public static String getTestData(String key) throws IOException, ParseException {
-        return (String) getJsonData().get(key);//return value
-
+    public static String getTestData(String key)  {
+        try {
+            return (String) getJsonData().get(key);//return value
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static JSONObject getJsonData() throws IOException, ParseException {
+    private static JSONObject getJsonData() {
         //pass the path of the testdata.json file
         File filename = new File("resources//testData//TestData.json");
         //convert json file into string
-        String json = FileUtils.readFileToString(filename, "UTF-8");
-        //parse the string into object
-        Object obj = new JSONParser().parse(json);
-        //give jsonobject so that I can return it to the function everytime it get called
-        JSONObject jsonObject = (JSONObject) obj;
+        String json;
+        JSONObject jsonObject;
+        try {
+            json = FileUtils.readFileToString(filename, "UTF-8");
+            //parse the string into object
+            Object obj = new JSONParser().parse(json);
+            //give jsonobject so that I can return it to the function everytime it get called
+            jsonObject = (JSONObject) obj;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return jsonObject;
-
     }
 
     public static JSONArray getJSONArrayData(String key) throws IOException, ParseException {
